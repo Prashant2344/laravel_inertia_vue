f7d41de087779d2660f82ca0907f714e77268d45
prashant-cyberarrow <prashant.silpakar@cyberarrow.io>
2024-08-16 18:14:15 +0545

    Removed time out

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::get();
-        sleep(3);
+        // sleep(3);
         return Inertia('Index',[
             'posts' => $posts
         ]);
